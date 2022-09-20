f5d700522b33e29a79645783133cf04394199f2c
imortimore <isaac.mortimore@internet.com>
2022-09-20 15:22:13 +0100

    initial push

---
+++ src/main/java/training/busboard/ClientAPI.java
@@ -0,0 +1,21 @@
+package training.busboard;
+
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Invocation;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jdk.nashorn.internal.runtime.Source;
+import org.glassfish.jersey.jackson.JacksonFeature;
+
+public class ClientAPI {
+    Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
+    WebTarget webTarget = client.target("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals");
+    Invocation.Builder invocationbuilder = webTarget.request(MediaType.APPLICATION_JSON);
+    Response response = invocationbuilder.get();
+
+    public Response getResponse() {
+        return response;
+    }
+}
--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -2,6 +2,11 @@ package training.busboard;
 
 public class Main {
     public static void main(String args[]) {
+
+ClientAPI test = new ClientAPI();
+
+        System.out.println(test.getResponse());
         // Your code here!
+
     }
 }	
