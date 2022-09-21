4d4705dd56993592a73ea468facfed1382068c72
imortimore <isaac.mortimore@btinternet.com>
2022-09-21 11:27:51 +0000

    getters set

--- src/main/java/training/busboard/BustopResponse.java
+++ src/main/java/training/busboard/BustopResponse.java
@@ -1,4 +1,7 @@
 package training.busboard;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.ws.rs.core.Response;
@@ -6,6 +9,14 @@ import jakarta.ws.rs.core.Response;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class BustopResponse {
     private String id;
+    private String stationName;
+    private String lineId;
+    private String destinationName;
+    private Integer timeToStation;
+    private LocalDateTime timestamp;
+    private LocalDateTime expectedArrival;
+
+
 
     public BustopResponse(){
     }
@@ -13,6 +24,31 @@ public class BustopResponse {
     public String getid(){
         return id;
     }
+    public String getStationName(){
+        return stationName;
+    }
+    public String getLineId(){
+        return lineId;
+    }
+    public String getDestinationName(){
+        return destinationName;
+    }
+    public Integer getTimeToStation(){
+        return timeToStation;
+    }
+    public LocalDateTime getTimeStamp(){
+        return timestamp;
+    }
+    public LocalDateTime getExpectedArrival(){
+        return expectedArrival;
+    }
+
+
+
+    
+    
+    
+    
     public void setid(String id){
         this.id = id;
     }
